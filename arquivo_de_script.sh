c43836a959cb6935d1fa8041bfbda952c8ca3b6f
fdlima <fdlima@gmail.com>
2014-04-03 18:02:40 -0300

    Update arquivo_de_script.sh
    new version: added the function to remove backup files

--- arquivo_de_script.sh
+++ arquivo_de_script.sh
@@ -8,4 +8,6 @@ name=`basename $1 .chk`
 
 cubegen 0 Density=SCF ${name}.fchk ${name}.den.cub 0 h
 cubegen 0 Potential=SCF ${name}.fchk ${name}.esp.cub 0 h
+
+rm teste
 ~                                                                  
